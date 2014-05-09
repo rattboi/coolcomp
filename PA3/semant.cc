2c814d1894f8cb15b3f56f89380f04c20988c5ee
Bradon Kanyid <rattboi@gmail.com>
2014-05-09 00:11:02 -0700

    73/74
    Fixed a bug with SELF_TYPE in new__class traversal

--- PA3/semant.cc
+++ PA3/semant.cc
@@ -953,6 +953,10 @@ Symbol string_const_class::traverse(ClassTable* env) {
 
 Symbol new__class::traverse(ClassTable* env) {
 
+    if (type_name == SELF_TYPE) {
+        return set_type(SELF_TYPE)->get_type();
+    }
+
     if (env->class_lookup.find(type_name) == env->class_lookup.end()) {
         env->semant_error(env->get_curr_class()) << "Undefined type in NEW" << endl;
         return set_type(Object)->get_type();
