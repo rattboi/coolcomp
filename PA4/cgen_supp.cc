f1fcc2e422a9e76918be6928e0c0fd8ec61cb06b
Bradon Kanyid <rattboi@gmail.com>
2014-05-20 22:20:06 -0700

    Really? Another whitespace commit? YES

--- PA4/cgen_supp.cc
+++ PA4/cgen_supp.cc
@@ -7,16 +7,16 @@ static int ascii = 0;
 
 void ascii_mode(ostream& str)
 {
-  if (!ascii) 
+  if (!ascii)
     {
       str << "\t.ascii\t\"";
       ascii = 1;
-    } 
+    }
 }
 
 void byte_mode(ostream& str)
 {
-  if (ascii) 
+  if (ascii)
     {
       str << "\"\n";
       ascii = 0;
@@ -46,12 +46,12 @@ void emit_string_constant(ostream& str, char* s)
       str << "\\\"";
       break;
     default:
-      if (*s >= ' ' && ((unsigned char) *s) < 128) 
+      if (*s >= ' ' && ((unsigned char) *s) < 128)
 	{
 	  ascii_mode(str);
 	  str << *s;
 	}
-      else 
+      else
 	{
 	  byte_mode(str);
 	  str << "\t.byte\t" << (int) ((unsigned char) *s) << endl;
